7b0f23dea98afbc1d2a12c648b5bcee084e604c5
hjk216 <62774444+hjk216@users.noreply.github.com>
2022-02-28 12:39:45 -0500

    changed input

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ async function main() {
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const Token = await ethers.getContractFactory("CraigCoin");
-    const token = await Token.deploy();
+    const token = await Token.deploy(deployer.address);
 
     console.log("Token address:", token.address);
 }
